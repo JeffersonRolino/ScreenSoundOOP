6d73346aac0214b15c84f9c1805dda249df4cde9
jeffersonrolino <jefferson.rolino@gmail.com>
2024-12-12 11:17:07 -0300

    Capítulo 03 - Aula 04: Exibindo as músicas de um álbum

--- ScreenSoundOOP/Program.cs
+++ ScreenSoundOOP/Program.cs
@@ -2,10 +2,14 @@
 albumDoQueen.Nome = "A Night at the Opera";
 
 Musica musica1 = new Musica();
-musica1.Nome = "Love of my live";
+musica1.Nome = "Love of my life";
+musica1.Duracao = 213;
 
 Musica musica2 = new Musica();
 musica2.Nome = "Bohemian Rhapsody";
+musica2.Duracao = 354;
 
 albumDoQueen.AdicionarMusica(musica1);
 albumDoQueen.AdicionarMusica(musica2);
+
+albumDoQueen.ExibirMusicasDoAlbum();
