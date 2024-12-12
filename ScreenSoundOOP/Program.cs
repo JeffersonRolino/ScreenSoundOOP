04133a1ecbb288855706d50a7dfd8d7cbba77c6e
jeffersonrolino <jefferson.rolino@gmail.com>
2024-12-12 09:30:36 -0300

    Capítulo 01 - Aula 04: Objetos

--- ScreenSoundOOP/Program.cs
+++ ScreenSoundOOP/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Musica musica1 = new Musica();
+musica1.nome = "Bohemian Rhapsody";
+musica1.artista = "Queen";
+musica1.duracao = 355;
+musica1.disponivel = true;
+
+Musica musica2 = new Musica();
+musica2.nome = "Here comes the sun";
+musica2.artista = "The Beatles";
+musica2.duracao = 185;
+musica2.disponivel = true;
+
+Console.WriteLine($"Nome: {musica1.nome}");
+Console.WriteLine($"Artista: {musica1.artista}");
