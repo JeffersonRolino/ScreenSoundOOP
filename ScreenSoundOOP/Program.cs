38ffe13e002884f099d1e51eb0b48f73ff32de3a
jeffersonrolino <jefferson.rolino@gmail.com>
2024-12-12 18:48:25 -0300

    Capítulo 05 - Aula 03: Resolvendo o desafio 1

--- ScreenSoundOOP/Program.cs
+++ ScreenSoundOOP/Program.cs
@@ -1,32 +1,5 @@
-﻿Banda queen = new Banda("Queen");
-
-Album albumDoQueen = new Album("A Night at the Opera");
-
-Musica musica1 = new Musica(queen, "Love of my life")
-{
-    Duracao = 213,
-    Disponivel = true
-};
-
-Musica musica2 = new Musica(queen, "Bohemian Rhapsody")
-{
-    Duracao = 354,
-    Disponivel = false
-};
-
-// Adicionando músicas ao Álbum;
-albumDoQueen.AdicionarMusica(musica1);
-albumDoQueen.AdicionarMusica(musica2);
-
-// Exibindo Álbum
-albumDoQueen.ExibirMusicasDoAlbum();
-
-
-// Exibir Músicas
-musica1.ExibirFichaTecnica();
-musica2.ExibirFichaTecnica();
-
-// Adicionando Álbum a Banda
-queen.AdicionarAlbum(albumDoQueen);
-// Exibir Discografia da Banda
-queen.ExibirDiscografia();
+﻿Episodio episodio1 = new Episodio(1, "Técnicas de Facilitação", 45);
+episodio1.AdicionarConvidados("Maria");
+episodio1.AdicionarConvidados("Pedro");
+episodio1.AdicionarConvidados("João");
+Console.WriteLine(episodio1.Resumo);
