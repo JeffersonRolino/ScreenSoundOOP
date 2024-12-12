5b468a80a6df62695c1b2d8b42a81c5adee970ea
jeffersonrolino <jefferson.rolino@gmail.com>
2024-12-12 11:40:19 -0300

    Capítulo 04 - Aula 04: Relacionamento entre as classes

--- ScreenSoundOOP/Musica.cs
+++ ScreenSoundOOP/Musica.cs
@@ -1,10 +1,15 @@
 ﻿class Musica
 {
     public string Nome { get; set; }
-    public string Artista { get; set; }
+    public Banda Artista { get; }
     public int Duracao {  get; set; }
     public bool Disponivel {  get; set; }
     public string DescricaoResumida => $"A música {Nome} pertence a banda {Artista}";
+
+    public Musica(Banda artista)
+    {
+        Artista = artista;
+    }
 
     public void ExibirFichaTecnica()
     {
