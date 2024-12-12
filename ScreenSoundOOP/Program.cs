8e0b9f0fa157b3280f2a4f21052c5bbee4fc271b
jeffersonrolino <jefferson.rolino@gmail.com>
2024-12-12 10:16:53 -0300

    Capítulo 02 - Aula 05: Atributos e propriedades

--- ScreenSoundOOP/Program.cs
+++ ScreenSoundOOP/Program.cs
@@ -1,13 +1,13 @@
 ﻿Musica musica1 = new Musica();
-musica1.nome = "Bohemian Rhapsody";
-musica1.artista = "Queen";
-musica1.duracao = 355;
+musica1.Nome = "Bohemian Rhapsody";
+musica1.Artista = "Queen";
+musica1.Duracao = 355;
 musica1.Disponivel = true;
 
 Musica musica2 = new Musica();
-musica2.nome = "Here comes the sun";
-musica2.artista = "The Beatles";
-musica2.duracao = 185;
+musica2.Nome = "Here comes the sun";
+musica2.Artista = "The Beatles";
+musica2.Duracao = 185;
 musica2.Disponivel = false;
 
 musica1.ExibirFichaTecnica();
